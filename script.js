868f29b41cf7aa9c38fa0a27e10bcf972f37bf50
Justin Berg <justin.berg1@verizon.com>
2024-09-10 10:10:48 -0500

    pushing the finished code

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Write your code here
+let bulb1 = document.querySelector('#lightbulb1');
+let bulb2 = document.querySelector('#lightbulb2');
+let bulb3 = document.querySelector('#lightbulb3');
+
+let subtitile = document.querySelector('.subtitle');
+let count = 0;
+
+bulb1.addEventListener('click', function() {
+    count ++;
+    subtitile.innerHTML = `You've clicked the lights ${count} times`
+    this.classList.toggle('active')
+});
+
+bulb2.addEventListener('click', function() {
+    count ++;
+    subtitile.innerHTML = `You've clicked the lights ${count} times`
+    this.classList.toggle('active')
+});
+
+bulb3.addEventListener('click', function() {
+    count ++;
+    subtitile.innerHTML = `You've clicked the lights ${count} times`
+    this.classList.toggle('active')
+});
+
